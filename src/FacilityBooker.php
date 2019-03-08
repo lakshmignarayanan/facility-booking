80be2fcf80bd0bc107d03a092fc381ad35038580
Lakshminarayanan <lakshmi.narayanan@gopassport.com>
2019-03-08 15:06:36 +0530

    calc rate and book if avl.

--- src/FacilityBooker.php
+++ src/FacilityBooker.php
@@ -1,53 +1,53 @@
 <?php
 
+require_once('Utils.php');
+
 Class FacilityBooker
 {
-
-	protected $filePath = 'bookings.csv';
+	protected $filePath = "bookings.csv";
 	protected $fileHandle = null;
 	protected $facilityId = 0;
-	protected $startTime, $endTime;
+	protected $startDateTime, $endDateTime;
 
-	private $facilities = array(
+	private $facilities = [
 		'1' => 'Club House',
 		'2' => 'Tennis Court',
 		'3' => 'Meeting Hall',
 		'4' => 'Gym'
-	);
+	];
 
 	// facility_id <=> ratetype
-	private $facilityRateType = array(
+	private $facilityRateType = [
 		'1' => 'variable',
 		'2' => 'fixed',
 		'3' => 'variable',
 		'4' => 'variable'
-	);
+	];
 
-	private $slots = array(
-		'0' => '2017-10-10 00:00:00_to_2017-10-10 23:59:00',
-		'1' => '2017-10-10 10:00:00_to_2017-10-10 16:00:00',
-		'2' => '2017-10-10 16:00:00_to_2017-10-10 22:00:00',
-		'4' => '2017-10-10 09:00:00_to_2017-10-10 17:00:00',
-		'5' => '2017-10-10 06:00:00_to_2017-10-10 09:00:00',
-		'6' => '2017-10-10 17:00:00_to_2017-10-10 21:00:00'
-	);
+	// slotid <=> duration
+	private $slots = [
+		'0' => '00:00:00_to_24:00:00',
+		'1' => '10:00:00_to_16:00:00',
+		'2' => '16:00:00_to_22:00:00',
+		'3' => '16:00:00_to_22:00:00',
+		'4' => '09:00:00_to_17:00:00',
+		'5' => '06:00:00_to_09:00:00',
+		'6' => '17:00:00_to_21:00:00'
+	];
 
 	//slotId <=> rateAmount
-	private $ratesPerHour = array(
+	private $ratesPerHour = [
 		'0' => 50,
 		'1' => 100, 
-		'2' => 500
-	);
-
-	//slotId <=> rateAmount
-	private $ratesPerMinute = [
-		'0' => 0.83, // 50/hr
-		'1' => 1.66,
-		'2' => 8.33
+		'2' => 500,
+		'3' => 20,
+		'4' => 70,
+		'5' => 1000,
+		'6' => 33
 	];
 
 	// facilityId <=> slot IDs
-	private $facilitiesSlots = [
+	private $facilitySlots = [
 		'1' => [
 			1, 2
 		],
@@ -63,7 +63,7 @@ Class FacilityBooker
 	];
 
 	function __construct($args) {
-		$this->fileHandle = fopen($filePath, 'r');
+		$this->fileHandle = fopen($this->filePath, 'r');
 		$this->sanitizeInput($args);
 	}
 
@@ -73,7 +73,7 @@ Class FacilityBooker
 			return 0;
 		}
 
-		foreach ($facilities as $key => $value) {
+		foreach ($this->facilities as $key => $value) {
 			if ($facility_name === $value) {
 				return intval($key);
 			}
@@ -83,94 +83,158 @@ Class FacilityBooker
 
 	public function sanitizeInput($input = '') {
 		if (count($input) < 2) {
-			throw new Exception("Empty input values", 1);
+			exit("Empty input values");
 		}
+		// error_log("input : " . json_encode($input));
 		$values = explode(",", $input[1]);
-		if (count($values != 4) || empty($values[1]) || empty($values[2]) || empty($values[3])) {
-			throw new Exception("Error Processing Request", 1);
+		if (count($values) != 4 || empty($values[1]) || empty($values[2]) || empty($values[3])) {
+			// error_log("hi values");
+			// print_r($values);
+			exit("Please check your input details");
 		} elseif ($this->getFacilityId($values[0]) == 0) {
-			throw new Exception("Facility doesn't exist", 1);
-		} elseif (!$validateDateString($values[1] . ' ' . $values[2] . ':00')) {
-			throw new Exception("Invalid start time", 1);
-		} elseif (!$validateDateString($values[1] . ' ' . $values[3] . ':00')) {
-			throw new Exception("Invalid end time", 1);
+			exit("Facility " . $values[0] . "doesn't exist");
+		} elseif (!Utils::validateDateString($values[1] . ' ' . $values[2] . ':00')) {
+			exit("Invalid start time");
+		} elseif (!Utils::validateDateString($values[1] . ' ' . $values[3] . ':00')) {
+			exit("Invalid end time");
 		}
 		$this->facilityId = $this->getFacilityId($values[0]);
-		$this->startTime = getFormattedDateTime($values[1] . ' ' . $values[2] . ':00');
-		$this->endTime = getFormattedDateTime($values[1] . ' ' . $values[3] . ':00');
+		$this->startDateTime = Utils::formatDateTime($values[1] . ' ' . $values[2] . ':00');
+		$this->endDateTime = Utils::formatDateTime($values[1] . ' ' . $values[3] . ':00');
 	}
 
 	public function processBooking() {
 		// check facility avl
-		if (!isFacilityAvailable($this->facilityId, $this->startTime, $this->endTime)) {
-			throw new Exception("Sorry, the facility is already booked!", 1);
+		if (!$this->isFacilityAvailable($this->facilityId, $this->startDateTime, $this->endDateTime)) {
+			exit("Sorry, the facility is already booked!");
 		} else {
 			// book
-			$rate = $this->calculateRate($this->facilityId, $this->startTime, $this->endTime);
-			$appendHandle = fopen($filePath, "a");
-			fputcsv($appendHandle, array($this->facilityId, $this->startTime, $this->endTime));
-			fclose($appendHandle);
-			return "Booked, Rs." . $rate;
+			$startTime = Utils::formatDateTime($this->startDateTime, 'H:i:s');
+			$endTime = Utils::formatDateTime($this->endDateTime, 'H:i:s');
+			$rate = $this->calculateRate($this->facilityId, $startTime, $endTime);
+			if ($rate > 0) {
+				$appendHandle = fopen($this->filePath, "a");
+				$data = "\n" . $this->facilityId . "," . $this->startDateTime . "," . $this->endDateTime;
+				fwrite($appendHandle, $data);
+				return "Booked, Rs." . $rate;	
+			} else {
+				return "Booking failed, try someother options";
+			}
+			
 		}
 	}
 
 	public function isFacilityAvailable($facility_id, $start_time, $end_time) {
 		if (!is_null($this->fileHandle)) {
-			$handle = $this->fileHandle;
-			$available = false;
-			while (($bookings === fgetcsv($handle)) !== false) {
-				$facilityHasBookings = false;
-				$bookedFacility = $bookings[0];
+			$available = true;
+			$facilityHasBookings = false;
+			while (($bookings = fgetcsv($this->fileHandle)) !== false) {
+				$bookedFacility = intval($bookings[0]);
 				$bookedStartTime = $bookings[1];
 				$bookedEndTime = $bookings[2];
-				if ($bookedFacility === $facility_id) {
+				error_log("\nbookedFacility=".$bookedFacility." bookedStartTime=".$bookedStartTime." bookedEndTime=".$bookedEndTime);
+				error_log("facility_id=".$facility_id." start_time=".$start_time." end_time=".$end_time);
+				if ($bookedFacility == $facility_id) {
+					error_log($bookedFacility . " == " . $facility_id);
 					$facilityHasBookings = true;
-					if (($start_time < $bookedStartTime && $end_time < $bookedEndTime) || ($start_time > $bookedStartTime && $end_time > $bookedEndTime)) {
-						return true;
+					if ($start_time > $bookedStartTime && $start_time < $bookedEndTime) {
+						error_log($bookedStartTime . " < " . $start_time . " < " . $bookedEndTime);
+					}
+					if ((strtotime($start_time) >= strtotime($bookedStartTime) && strtotime($end_time) <= strtotime($bookedEndTime)) || (strtotime($start_time) >= strtotime($bookedStartTime) && strtotime($start_time) < strtotime($bookedEndTime)) || (strtotime($end_time) > strtotime($bookedStartTime) && strtotime($end_time) <= strtotime($bookedEndTime))) {
+						error_log("\nslot booked..");
+						$available = false;
+						break;
 					}
 				}
 			}
-			if (!$facilityHasBookings) {
+			if (!$facilityHasBookings || $available) {
 				return true;
 			}
+			return false;
 		} else {
-			throw new Exception("Error handling previous bookings", 1);
+			exit("Error handling previous booking details");
 		}
 	}
 
 	// returns rate
 	public function calculateRate($facility_id, $start_time, $end_time) {
 		// check if variable rate
-		if ($facilityRateType[$facility_id] == 'fixed') {
-			$minutesToBook = getTimeDifferenceInMinutes($start_time, $end_time);
-			return ceil($ratesPerMinute[0] * $minutesToBook);
+		if ($this->facilityRateType[$facility_id] === 'fixed') {
+			$hoursToBook = Utils::getTimeDifferenceInHours($start_time, $end_time);
+			return ceil($this->ratesPerHour[0] * $hoursToBook);
 		} else {
-			// divide and handle slots
-			
+			// get the slots
+			$slotsApplicable = $this->facilitySlots[$facility_id];
+			$shortlistedSlots = [];
+			// $totalRate = 0;
+			error_log("slotsApplicable = " . json_encode($slotsApplicable));
+			foreach ($slotsApplicable as $slotid) {
+				$shortlistedSlots[$slotid] = $this->slots[$slotid];
+			}
+			error_log("shortlistedSlots=".json_encode($shortlistedSlots));
+			foreach ($shortlistedSlots as $slotId => $slotTimings) {
+				$slotStart = explode("_to_", $slotTimings)[0];
+				$slotEnd = explode("_to_", $slotTimings)[1];
+				error_log("slotStart=".$slotStart."/slotEnd=".$slotEnd."/start_time=".$start_time."/end_time=".$end_time);
+				if ($start_time >= $slotStart && $end_time <= $slotEnd) {
+					error_log("falls in block");
+					// falls within slot block
+					$ratePerHr = $this->ratesPerHour[$slotId];
+					return ceil($ratePerHr * Utils::getTimeDifferenceInHours($start_time, $end_time));
+				} elseif ($start_time > $slotStart && $start_time < $slotEnd && $end_time > $slotEnd) {
+					error_log("stretches rightwards..");
+					// booking time stretches rightwards out of the slots
+					// calc 1st part
+					$intialHrs = Utils::getTimeDifferenceInHours($start_time, $slotEnd);
+					error_log("intialHrs = " . $intialHrs);
+					$ratePerHr = $this->ratesPerHour[$slotId];
+					error_log("ratePerHr=" . $ratePerHr . " for slotid" . $slotId);
+					$totalRate = $intialHrs * $ratePerHr;
+					foreach ($shortlistedSlots as $slotId2 => $slotTimings2) {
+						$slotStart2 = explode("_to_", $slotTimings2)[0];
+						$slotEnd2 = explode("_to_", $slotTimings2)[1];
+						if ($start_time < $slotStart2 && $end_time > $slotStart2 && $end_time < $slotEnd2) {
+							error_log("slotStart2=".$slotStart2."/slotEnd2=".$slotEnd2."/start_time=".$start_time."/end_time=".$end_time);
+							// calc the remaining part of the duration that stretches leftward
+							$laterHrs = Utils::getTimeDifferenceInHours($slotStart2, $end_time);
+							error_log("laterHrs=".$laterHrs);
+							error_log("ratePerHr = " . $this->ratesPerHour[$slotId2] . " for slot " . $slotId2);
+							error_log("previous rate = " . $totalRate);
+							error_log("additional rate= " . $laterHrs * $this->ratesPerHour[$slotId2]);
+							$totalRate = $totalRate + ($laterHrs * $this->ratesPerHour[$slotId2]);
+							return $totalRate;
+						}
+					}
+				} elseif ($start_time < $slotStart && $end_time > $slotStart && $end_time < $slotEnd) {
+					error_log("stretches leftwards..");
+					// slot stretches leftwards first
+					$laterHrs = Utils::getTimeDifferenceInHours($slotStart, $end_time);//calc rightmost part
+					error_log("laterHrs=".$laterHrs);
+					$ratePerHr = $this->ratesPerHour[$slotId];
+					error_log("ratePerHr=".$ratePerHr." for slotId" . $slotId);
+					$totalRate = $laterHrs * $ratePerHr;
+					foreach ($shortlistedSlots as $slotId2 => $slotTimings2) {
+						$slotStart2 = explode("_to_", $slotTimings2)[0];
+						$slotEnd2 = explode("_to_", $slotTimings2)[1];
+						if ($start_time < $slotEnd2 && $start_time > $slotStart2 && $end_time > $slotEnd2) {
+							// calc the leftmost part
+							error_log("slotStart2=".$slotStart2."/slotEnd2=".$slotEnd2."/start_time=".$start_time."/end_time=".$end_time);
+							$initHrs = Utils::getTimeDifferenceInHours($start_time, $slotEnd2);
+							$ratePerHr = $this->ratesPerHour[$slotId];
+							$totalRate = $totalRate + ($initHrs * $ratePerHr);
+							return $totalRate;
+						}
+					}
+				} else {
+					error_log("lies in an unknown block");
+					return 0;
+				}
+			}
+
 		}
 	}
 
-	public function getFormattedDateTime($datetime, $format = 'Y-m-d H:i:s') {
-		$d = DateTime::createFromFormat($format, $datetime);
-		return $d->format($format);
-	}
-
-	public static function validateDateString($date, $format = 'Y-m-d H:i:s') {
-        $d = DateTime::createFromFormat($format, $date);
-        return ($d && ($d->format($format) == $date));
-    }
-
-    public static function getTimeDifferenceInMinutes($start_time, $end_time) {
-    	$start_date = new DateTime($start_time);
-		$since_start = $start_date->diff(new DateTime($end_time));
-		$minutes = $since_start->days * 24 * 60;
-		$minutes += $since_start->h * 60;
-		$minutes += $since_start->i;
-		return $minutes;
-    }
-
 }
 
-
-FacilityBooker $booker = new FacilityBooker($argv);
+$booker = new FacilityBooker($argv);
 echo $booker->processBooking();
---
+++ src/Utils.php
@@ -0,0 +1,21 @@
+<?php
+Class Utils {
+
+	public static function formatDateTime($dateTime, $format = 'Y-m-d H:i:s') {
+		$datetime = strtotime($dateTime);
+		return date($format, $datetime);
+	}
+
+	public static function validateDateString($date, $format = 'Y-m-d H:i:s') {
+        $d = DateTime::createFromFormat($format, $date);
+        return ($d && ($d->format($format) == $date));
+    }
+
+    public static function getTimeDifferenceInHours($start_time, $end_time) {
+    	$start_date = new DateTime($start_time);
+		$since_start = $start_date->diff(new DateTime($end_time));
+		$hours = $since_start->days * 24;
+		$hours += $since_start->h;
+		return $hours;
+    }
+}
